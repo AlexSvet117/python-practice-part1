e9089662872fdd84956431ec01f0e55028f375c5
AlexSvet117 <alex.svet117@gmail.com>
2025-03-05 00:50:09 +0000

    solution 03 - 07

---
+++ 06_none_means_something/solution.py
@@ -0,0 +1,6 @@
+full_name = None
+print(f"Initial full_name: {full_name}")
+
+full_name = "Bob Dylan"
+print(f"Updated full_name: {full_name}")
+print(f"full_name has been updated.")
---
+++ 07_type_detective/solution.py
@@ -0,0 +1,12 @@
+number = 100
+fl_number = 3.14
+compl_number = (3+4j)
+bl_boolean = True
+
+print(number, type(number))
+print(fl_number, type(fl_number))
+print(compl_number, type(compl_number))
+print(bl_boolean, type(bl_boolean))
+
+expression = 0.1 + 0.2 == 0.3
+print(f"0.1 + 0.2 == 0.3: {expression}")
